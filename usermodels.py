ba1963e147c5424afe086229c5fdc51c972f1131
Mark Cotton <tifosigoon@gmail.com>
2015-07-18 02:49:17 -0500

    adding form to create a new post

--- usermodels.py
+++ usermodels.py
@@ -5,8 +5,12 @@ class Blog(db.Model):
   date = db.DateTimeProperty(auto_now_add=True)
   title = db.StringProperty()
   tag = db.StringProperty()
+  author = db.StringProperty()
+  content_img = db.TextProperty() # Image above preview post
   content = db.TextProperty()  # Generated HTML from showdown.js
   markdown = db.TextProperty() # Markdown kept allow editting later
+  teaser = db.TextProperty() # Teaser that is shown on front page
+  show_teaser = db.BooleanProperty() # Should we show the whole post or just the teaser?
   
 class SideBar(db.Model):
   title = db.StringProperty()
